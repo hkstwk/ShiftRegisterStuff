7228f3e3d606cf605310e127fc70e3eb8c4e206e
hkstwk <hkolvoort+github@gmail.com>
2017-12-25 15:45:01 +0100

    created knight rider function

--- BlinkingLed/main.cpp
+++ BlinkingLed/main.cpp
@@ -23,19 +23,27 @@
 #define SH_CP_high() HC595_PORT |=  (1 << SH_CP_PIN)
 
 // Master Reclear using these two functions
-#define MR_low()  HC595_PORT &= ~(1 << MR_PIN)
-#define MR_high() HC595_PORT |=  (1 << MR_PIN)
+#define MR_low()  	HC595_PORT &= ~(1 << MR_PIN)
+#define MR_high() 	HC595_PORT |=  (1 << MR_PIN)
 
 // Output Enable using these two functions
-#define OE_low()  HC595_PORT &= ~(1 << OE_PIN)
-#define OE_high() HC595_PORT |=  (1 << OE_PIN)
+#define OE_low()  	HC595_PORT &= ~(1 << OE_PIN)
+#define OE_high() 	HC595_PORT |=  (1 << OE_PIN)
 
-#define DELAY	250
+#define DELAY		50
+
+// Number of bytes needed to store output pin.
+// Example: 16 (led)pins needed? Take two bytes = 2 x 8 = 16 bits
+#define PIN_BYTES	2
+
+volatile unsigned char ledPins[PIN_BYTES];
 
 //Define functions
 //===============================================
 void ioinit(void);
 void output_led_state(unsigned int __led_state);
+void setLedPins(unsigned char ledBytes[PIN_BYTES]);
+void knightRider(unsigned char loops);
 //===============================================
 
 int main (void)
@@ -44,21 +52,14 @@ int main (void)
 
    while(1)
    {
-      // Output a knight rider pattern
-
-      for (int i=15;i>=0;i--)
-      {
-         output_led_state(_BV(i) ^ 0b1111000011110000);
-         _delay_ms(DELAY);
-      }
-      for (int i=1;i<15;i++)
-      {
-         output_led_state(_BV(i) ^ 0b1111000011110000);
-         _delay_ms(DELAY);
-      }
+	   knightRider(0);
+	   _delay_ms(2000);
+	   knightRider(5);
+	   _delay_ms(2000);
    }
-}
 
+   return 0;
+}
 
 void ioinit (void)
 {
@@ -86,20 +87,45 @@ void output_led_state(unsigned int __led_state)
    ST_CP_high();
 }
 
-void setLedPins(unsigned int __led_state)
+/* This function sets 16 leds on or off. Depending on the value of the bit that represents the led.
+ * To save memory the bits are stored in a two byte unsigned integer.
+ * Setting the leds on or off is just a matter of iterating all bits
+ */
+void setLedPins(unsigned char ledBytes[PIN_BYTES])
 {
    SH_CP_low();
    ST_CP_low();
-   for (int i=0;i<16;i++)
-   {
-      if ((_BV(i) & __led_state) == _BV(i))  //bit_is_set doesn’t work on unsigned int so we do this instead
-         DS_high();
-      else
-         DS_low();
+   for (int byteCount=0; byteCount < PIN_BYTES; byteCount++){
+	   for (int i=0; i<8; i++)
+	   {
+		   if ((ledBytes[byteCount] & (1 << i)) == (1 << i))
+			 DS_high();
+		  else
+			 DS_low();
 
 
-      SH_CP_high();
-      SH_CP_low();
+		  SH_CP_high();
+		  SH_CP_low();
+	   }
+	   ST_CP_high();
    }
-   ST_CP_high();
+}
+
+void knightRider(unsigned char loops){
+	if (loops == 0)
+		loops = 1;
+	while (loops > 0){
+		for (int i=15;i>=0;i--)
+		{
+		   output_led_state(_BV(i) ^ 0b1111000011110000);
+		   _delay_ms(DELAY);
+		}
+		for (int i=1;i<15;i++)
+		{
+		   output_led_state(_BV(i) ^ 0b1111000011110000);
+		   _delay_ms(DELAY);
+		}
+		loops--;
+	}
+	output_led_state(_BV(15) ^ 0b1111000011110000);
 }
